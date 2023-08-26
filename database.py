c2afe61155b446c828e706c4f38e29a94e4c87ce
Linzh <linzh7@yahoo.com>
2023-08-26 13:58:09 +0300

    [F] fixed previous value

--- database.py
+++ database.py
@@ -16,15 +16,15 @@ class UserDB:
     def set_user_dict(self, user_id, key, value):
         if str(user_id) not in self.user_db.keys():
             self.user_db[str(user_id)] = {}
-        previous_value = self.user_db[str(user_id)].get(key)
-        if previous_value != value:
-            self.user_db[str(user_id)][key] = value
-            self.sync_user_db()
-            return previous_value
+        previous_value = self.user_db[str(user_id)][key] if key in self.user_db[str(user_id)].keys() else None
+        self.user_db[str(user_id)][key] = value
+        self.sync_user_db()
+        return previous_value
 
     def set_user_command(self, user_id, trigger, command):
-        self.set_user_dict(user_id, trigger, command)
+        previous_value = self.set_user_dict(user_id, trigger, command)
         self.sync_user_db()
+        return previous_value
 
     def get_user_command(self, user_id, trigger):
         if str(user_id) not in self.user_db.keys():
